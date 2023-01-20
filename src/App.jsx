273a6b1dde5313269a4faf34910814fc9ef9bf2d
Felipe Gustavo de Meneses Mendes <mendesf.dev@gmail.com>
2023-01-20 10:07:13 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Post } from "./Post";
+
+export function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Post author="Felipe Mendes" content="Bla" />
+      <Post author="Felipe Mendes" content="Novo post" />
+    </>
+  );
+}
